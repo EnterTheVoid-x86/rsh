f022ba2527359aa0b54c3313dfe7e3b65e375f73
EnterTheVoid <64978924+EnterTheVoid-x86@users.noreply.github.com>
2021-12-23 20:31:54 +0000

    fix?

--- modules/first_boot.sh
+++ modules/first_boot.sh
@@ -1,4 +1,5 @@
-echo "first_time: false
-  prompt: '~$ '
-  message: 'Welcome to rsh, v1.39. Enter “help” for help.'
-  ascii: 'logo.txt'" > config.yml
+echo "
+first_time: false
+prompt: '~$ '
+message: 'Welcome to rsh, v1.39. Enter “help” for help.'
+ascii: 'logo.txt'" > config.yml
