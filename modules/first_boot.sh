41958914729cd019e8b60eb9eacaad678fb4aa7b
EnterTheVoid <64978924+EnterTheVoid-x86@users.noreply.github.com>
2021-12-24 01:35:43 +0000

    more fixes

--- modules/first_boot.sh
+++ modules/first_boot.sh
@@ -1,4 +1,5 @@
 echo "first_boot: false
 prompt: '~$ '
 message: 'Welcome to rsh, v1.39. Enter “help” for help.'
+# Hey there! If you want to know how to change your ASCII art logo on startup, just simply replace the logo in logo.txt with your ASCII art! Got it?
 ascii: 'logo.txt'" > config.yml
